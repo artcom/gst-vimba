1d0ca530370a4cc6ec8e13f1ef81fb1354f9105c
Robert Chudoba <robert@artcom.de>
2015-06-03 12:38:06 +0200

    demo with larger size

--- showcamera.sh
+++ showcamera.sh
@@ -1,1 +1,1 @@
-gst-launch-1.0 vimbasrc camera=DEV_000F3102A408 ! video/x-raw,format=UYVY,width=1024,height=768 ! videoconvert ! ximagesink
+gst-launch-1.0 vimbasrc camera=DEV_000F3102A408 offset-x=308 offset-y=330 ! video/x-raw,format=UYVY,width=1920,height=1080 ! videoconvert ! ximagesink
